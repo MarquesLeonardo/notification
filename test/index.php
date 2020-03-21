1b695acf5a11ade22e3c12b4f0407b92a73da104
MarquesLeonardo <leonardo15dany@gmail.com>
2020-03-21 12:47:27 -0300

    index edit

--- test/index.php
+++ test/index.php
@@ -6,6 +6,6 @@ use Notification\Email;
 
 $email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls/ssl)", "port (587)", "from@email.com", "From Name");
 
-$email->sendMail("Testando a biblioteca", "<p>Se você estiver vendo esse <b>E-mail</b> é porque esta tudo configurado corretamente!</p>", "leonardomarquesdasilva1@gmail.com", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
+$email->sendMail("Testing the library", "<p>If you are seeing this <b> Email </b> it is because everything is set up correctly!</p>", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 
 var_dump($email);
